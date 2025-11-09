2b4df9a5da040f8a7210f1a7b403f19f78e3abd3
Subhankar <subhankardhal637@gmail.com>
2025-11-09 23:57:05 +0530

    Complete courses

--- script.js
+++ script.js
@@ -37,3 +37,27 @@ window.onscroll = () =>{
   }
 }
 
+let toggleBtn = document.querySelector('#toggle-btn');
+let darkMode = document.querySelector('dark-mode');
+
+const enableDarkMode = () =>{
+  toggleBtn.classList.replace('fa-sun', 'fa-moon');
+  body.classList.add('dark');
+  localStorage.setItem('dark-mode', 'enabled');
+}
+const disableDarkMode = () =>{
+  toggleBtn.classList.replace('fa-moon', 'fa-sun');
+  body.classList.remove('dark');
+  localStorage.setItem('dark-mode', 'disabled');
+}
+if (localStorage.getItem('dark-mode') === 'enabled'){
+  enableDarkMode();
+}
+toggleBtn.onclick = () =>{
+  let darkMode = localStorage.getItem('dark-mode');
+  if (darkMode === 'disabled'){
+    enableDarkMode();
+  }else{
+    disableDarkMode();
+  }
+}
